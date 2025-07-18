45274805a7e174146bdea3860ec599de8d33a0d0
Mateo Card <mateo.cardenas.pedraza@gmail.com>
2025-07-17 23:01:49 -0500

    commit 1

---
+++ SIAI/app/Models/Tech_Characteristics.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tech_Characteristics extends Model
+{
+    /** @use HasFactory<\Database\Factories\TechCharacteristicsFactory> */
+    use HasFactory;
+}
---
+++ SIAI/app/Models/Telefono_seller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Telefono_seller extends Model
+{
+    /** @use HasFactory<\Database\Factories\TelefonoSellerFactory> */
+    use HasFactory;
+}
